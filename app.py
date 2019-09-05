dfaf5dd8978e7e4b2651a4a9600c88f24b706ac7
jaebooker <jaesonbooker@gmail.com>
2019-09-05 16:25:34 -0700

    not third

--- app.py
+++ app.py
@@ -21,7 +21,11 @@ def index():
     Once you've found at least two (2) exploits, share your findings with a peer.
     Finally, fix this code so that this route is no longer exploitable.
     """
-    exploit = request.args.get('exploit')
+    exploit_filter = request.args.get('exploit')
+    if exploit_filter != None:
+        if "{{" or "<script>" in exploit_filter:
+            exploit_filter = "Oy! You there! What are you tring to do? Thought I wouldn't notice, didn't you?"
+    exploit = exploit_filter
     rendered_template = render_template("app.html", exploit=exploit)
     return render_template_string(rendered_template)
 
